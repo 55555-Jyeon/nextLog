c7e1d625d6e1d13404bf10344ddde95085536c47
55555-Jyeon <jyeon380516@gmail.com>
2024-03-27 00:01:56 +0900

    move folder

---
+++ src/app/types/post.ts
@@ -0,0 +1,8 @@
+export type TPost = {
+  title: string;
+  slug: string;
+  date: string;
+  image: string;
+  summary: string;
+  content: string;
+};
---
+++ src/app/types/props.ts
@@ -0,0 +1,13 @@
+export type TParams = {
+  params: {
+    slug: string;
+  };
+};
+
+export type TPostProps = {
+  title: string;
+  summary: string;
+  image: string;
+  slug: string;
+  date: string;
+};
